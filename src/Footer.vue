d38ddcaf41466f22b318b08a647a955141652d37
drd-2rcl <drd.rclrd@gmail.com>
2019-02-22 13:52:14 -0300

    :recycle: Refactoring code

---
+++ src/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="row">
+      <div class="col-xs-12">
+        <footer>
+          <p>All Server are managed here</p>
+        </footer>
+      </div>
+    </div>
+</template>
+
+<script></script>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Home from './Home.vue'
 
 // Vue.config.productionTip = false
-
-Vue.component('app-server-status', Home);
-
 
 new Vue({
   render: h => h(App),

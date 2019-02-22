8f2f6919e859ca3d6a0cf9422ee6c9f1d58c4980
drd-2rcl <drd.rclrd@gmail.com>
2019-02-22 10:36:36 -0300

    :tada: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Server Status: {{ status }} </p>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      status: 'Critical'
+    }
   }
 }
 </script>
---
+++ src/components/form/button.vue
@@ -0,0 +1,54 @@
+<template>
+    <button :class="classListFormated" @click="$emit('click', $event)" :type="type">OPA
+      <slot>
+        {{ buttonText }}
+      </slot>
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'button',
+    },
+    buttonText: {
+        type: String,
+        required: false,
+    },
+    classList: {
+        type: Array,
+        required: false,
+    },
+  },
+  data() {
+    return {
+      defaultClass: 'btn'
+    }
+  },
+  computed: {
+    classListFormated() {
+      return [].cancat(this.defaultClass, this.classList).join(' ')
+    },
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+
+.btn {
+  font-family: RobotoLight;
+  cursor: pointer;
+  padding: 6px 12px;
+  width: 220pc;
+  min-height: 40px;
+  color: var(--white);
+  background-color: #939598;
+  border: 1px solid #939598;
+  font-size: 1rem;
+  transition: .2s ease-in-out; 
+}
+</style>
